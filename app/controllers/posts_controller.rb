51b4b496b9519bce0b23bd3ba22bbe871f7b3ae7
Ash-x1 <ash91p@proton.me>
2026-03-28 21:17:40 +0100

    Add logic to show post creators to only signed_in users and create index html page to show the list of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-   # @posts = Post.all
+   @posts = Post.all
   end
   
   def new 
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
+ # Defines the root path route ("/")
   # root "posts#index"
   root "posts#new"
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:index, :new, :create]
 end
